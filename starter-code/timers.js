7dd2d99f50262c761c08957658eb7ee72ab37b5a
AZIZ YUSOFF <azizyusoff@AZIZ-YUSOFFs-MacBook-Pro.local>
2016-09-29 00:36:55 +0800

    updated 1236amm

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,67 @@
 // your code here:
+
+function init(){
+
+
+var seconds = 0;
+var timerID = 0;
+
+var reset = document.getElementById('reset');
+reset.addEventListener('click', resetIt);
+
+var start = document.getElementById('start');
+start.addEventListener('click', startIt);
+
+
+var pause = document.getElementById('pause');
+pause.addEventListener('click', pauseIt);
+
+
+
+
+  function updateTime(){
+       var h1timer = document.querySelector('#timer')
+  h1timer.textContent = 'Timer: ' + seconds + 's'
+  seconds++
+  }
+
+
+
+function resetIt(){
+   clearInterval(timerID);
+  timerID = 0
+   seconds = 0;
+  var h1timer = document.querySelector('#timer')
+   h1timer.textContent = "Stop Watch";
+}
+
+function startIt(){
+
+
+if (timerID === 0) {
+  timerID = setInterval(updateTime, 1000);
+  console.log(timerID);
+
+  }else{
+    timerID = clearInterval(pauseIt, 1000);
+  }
+
+
+
+
+}
+
+
+
+   function pauseIt(){
+       clearInterval(timerID);
+      timerID = 0
+  }
+
+
+
+}
+
+
+
+init();
